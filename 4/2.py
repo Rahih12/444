f5198f9241e2726636a2c7290693dabde628635b
Rahih12 <143862410+Rahih12@users.noreply.github.com>
2023-11-01 22:35:51 +0700

    Add files via upload

---
+++ 4/2.py
@@ -0,0 +1,7 @@
+﻿def sort(*args):
+    ns = sorted([num for num in args if num < 0], reverse=True)
+    ps = sorted([num for num in args if num >= 0])
+    return (ns, ps)
+
+r = sort(-5, 10, -3, 0, 8, -1)
+print(r)  # ([-1, -3, -5], [0, 8, 10])
---
+++ 4/3.py
@@ -0,0 +1,21 @@
+﻿def calculate_GDP(C, I, G, Ex, Im):
+    return C + I + G + Ex - Im
+
+values_list = []
+values_dict = {}
+
+C = int(input("Введите значение C: "))
+I = int(input("Введите значение I: "))
+G = int(input("Введите значение G: "))
+Ex = int(input("Введите значение Ex: "))
+Im = int(input("Введите значение Im: "))
+
+values_list = [C, I, G, Ex, Im]
+values_dict = {'C': C, 'I': I, 'G': G, 'Ex': Ex, 'Im': Im}
+
+def print_GDP(*args, **kwargs):
+    GDP = calculate_GDP(*args, **kwargs)
+    print("GDP:", GDP)
+
+print_GDP(*values_list)
+print_GDP(**values_dict)
